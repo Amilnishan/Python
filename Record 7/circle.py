806154e8d5d0093463bacbb4e95988afb9b209fd
Amilnishan <amilnishan7@gmail.com>
2024-12-10 12:03:02 +0530

    Completed

---
+++ Record 7/circle.py
@@ -0,0 +1,7 @@
+import math
+
+def circlearea(r) :
+    return math.pi*r*r
+    
+def circleperi(r) :
+    return 2*math.pi*r
---
+++ Record 7/package/Circle1.py
@@ -0,0 +1,7 @@
+import math
+
+def circlearea(r) :
+    return math.pi*r*r
+    
+def circleperi(r) :
+    retutn 2*math.pi*r
